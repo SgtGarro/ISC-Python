4af1b3221800dc1d5487bce08f46707d51e6b5cd
SgtGarro <marcelogarro137@gmail.com>
2022-11-18 18:37:07 -0500

    Chapter 1

---
+++ chapter2/main.py
@@ -0,0 +1,16 @@
+print(42 % 10)
+
+x = 1 + 2 * 3 - 8 / 4
+print(x)
+
+y = int(98.6)
+print(y)
+
+# 2.2
+name = input("Enter your name")
+print("Hello " + name)
+
+# 2.3
+hrs = input("Enter hours: ")
+pay = float(hrs) * 2.75
+print("Pay: " + str(pay))
